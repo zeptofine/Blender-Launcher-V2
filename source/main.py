f552165d09e117aab67b01ba53449c8c032e6608
zeptofine <xpsychonauticonx@gmail.com>
2024-03-23 21:50:48 -0400

    split up the different modes into functions

--- source/main.py
+++ source/main.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import gettext
 import logging
 import os
-import shutil
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
@@ -11,6 +10,7 @@ from pathlib import Path
 import modules._resources_rc
 from modules import argument_parsing as ap
 from modules._platform import _popen, get_cwd, get_launcher_name, get_platform, is_frozen
+from PyQt5.QtWidgets import QApplication
 
 version = "2.0.24"
 
@@ -37,20 +37,6 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 sys.excepthook = handle_exception
 
 
-# List of available line arguments
-cli_help = """
-    Command line arguments sheet:
-
-    -help                        * Show command line arguments sheet
-    -update                      * Run updater instead of the main application
-    -debug                       * Set logging level to DEBUG
-    -set-library-folder "%path%" * Set library folder
-    -offline                     * Disable scraper thread
-    -instanced                   * Do not check if other BL instance is
-                                   running, used for restarting app
-    """
-
-
 def add_help(parser: ArgumentParser):
     parser.add_argument(
         parser.prefix_chars + "h",
@@ -75,13 +61,13 @@ def main():
     parser.add_argument(
         "--offline",
         "-offline",
-        help="Run the application offline. (Disables the scraper threads and update checks)",
+        help="Run the application offline. (Disables scraper threads and update checks)",
         action="store_true",
     )
     parser.add_argument(
         "--instanced",
         "-instanced",
-        help="Run the application in an already running instance.",
+        help="Do not check for existing instance.",
         action="store_true",
     )
 
@@ -107,14 +93,6 @@ def main():
     else:
         logging.root.setLevel(logging.INFO)
 
-    from modules.settings import set_library_folder
-    from PyQt5.QtCore import QByteArray
-    from PyQt5.QtNetwork import QLocalSocket
-    from PyQt5.QtWidgets import QApplication
-    from windows.dialog_window import DialogWindow
-    from windows.main_window import BlenderLauncher
-    from windows.update_window import BlenderLauncherUpdater
-
     # Create an instance of application and set its core properties
     app = QApplication([])
     app.setStyle("Fusion")
@@ -122,56 +100,81 @@ def main():
 
     set_lib_folder: Path | None = args.set_library_folder
     if set_lib_folder is not None:
-        if set_library_folder(str(set_lib_folder)):
-            logging.info(f"Library folder set to {set_lib_folder!s}")
-        else:
-            logging.error("Failed to set library folder")
-            dlg = DialogWindow(
-                title="Warning",
-                text="Passed path is not a valid folder or<br>it doesn't have write permissions!",
-                accept_text="Quit",
-                cancel_text=None,
-                app=app,
-            )
-            dlg.show()
-            sys.exit(app.exec())
+        start_set_library_folder(app, str(set_lib_folder))
 
     if args.command == "update":
-        if args.instanced or not is_frozen():
-            BlenderLauncherUpdater(app=app, version=version, release_tag=args.version)
-            sys.exit(app.exec())
-        else:
-            # Copy the launcher to the updater position
-            bl_exe, blu_exe = get_launcher_name()
-            cwd = get_cwd()
-            source = cwd / bl_exe
-            dist = cwd / blu_exe
-            shutil.copy(source, dist)
-
-            # Run the updater with the instanced flag
-            if get_platform() == "Windows":
-                _popen([blu_exe, "--instanced", "update"])
-            elif get_platform() == "Linux":
-                os.chmod(blu_exe, 0o744)
-                _popen(f'nohup "{blu_exe}" --instanced update')
-            sys.exit(0)
+        start_update(app, args.instanced, args.version)
 
     # if args.command == "launch":
     #     ...
 
     if not args.instanced:
-        socket = QLocalSocket()
-        socket.connectToServer("blender-launcher-server")
-        is_running = socket.waitForConnected()
-        if is_running:
-            socket.write(QByteArray(version.encode()))
-            socket.waitForBytesWritten()
-            socket.close()
-            return
+        check_for_instance()
+
+    from windows.main_window import BlenderLauncher
 
     app.setQuitOnLastWindowClosed(False)
+
     BlenderLauncher(app=app, version=version, offline=args.offline)
     sys.exit(app.exec())
+
+
+def start_set_library_folder(app: QApplication, lib_folder: str):
+    from modules.settings import set_library_folder
+    from windows.dialog_window import DialogWindow
+
+    if set_library_folder(str(lib_folder)):
+        logging.info(f"Library folder set to {lib_folder!s}")
+    else:
+        logging.error("Failed to set library folder")
+        dlg = DialogWindow(
+            title="Warning",
+            text="Passed path is not a valid folder or<br>it doesn't have write permissions!",
+            accept_text="Quit",
+            cancel_text=None,
+            app=app,
+        )
+        dlg.show()
+        sys.exit(app.exec())
+
+
+def start_update(app: QApplication, is_instanced: bool, tag: str | None):
+    import shutil
+
+    from windows.update_window import BlenderLauncherUpdater
+
+    if is_instanced or not is_frozen():
+        BlenderLauncherUpdater(app=app, version=version, release_tag=tag)
+        sys.exit(app.exec())
+    else:
+        # Copy the launcher to the updater position
+        bl_exe, blu_exe = get_launcher_name()
+        cwd = get_cwd()
+        source = cwd / bl_exe
+        dist = cwd / blu_exe
+        shutil.copy(source, dist)
+
+        # Run the updater with the instanced flag
+        if get_platform() == "Windows":
+            _popen([blu_exe, "--instanced", "update"])
+        elif get_platform() == "Linux":
+            os.chmod(blu_exe, 0o744)
+            _popen(f'nohup "{blu_exe}" --instanced update')
+        sys.exit(0)
+
+
+def check_for_instance():
+    from PyQt5.QtCore import QByteArray
+    from PyQt5.QtNetwork import QLocalSocket
+
+    socket = QLocalSocket()
+    socket.connectToServer("blender-launcher-server")
+    is_running = socket.waitForConnected()
+    if is_running:
+        socket.write(QByteArray(version.encode()))
+        socket.waitForBytesWritten()
+        socket.close()
+        sys.exit()
 
 
 if __name__ == "__main__":
