bc4cf410e75cfa1700e73588516c34072cd01ac6
zeptofine <xpsychonauticonx@gmail.com>
2024-07-19 10:58:24 -0400

    directory patches for linux scripts/

--- scripts/mkdocs_publish.sh
+++ scripts/mkdocs_publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cd ..
-cd docs
+# check if we need to move back to get to the docs folder
+if [ "$(basename "$PWD")" = "scripts" ]; then
+    cd ../docs || exit
+else
+    cd ./docs || exit
+fi
+
 mkdocs gh-deploy
--- scripts/zip_linux.sh
+++ scripts/zip_linux.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
-cd..
+# check if we need to move back to the root of the project folder
+if [ "$(basename "$PWD")" = "scripts" ]; then
+    cd ..
+fi
 
 # get the distro and get the Title version
 distro_id=$(python3 -c "import distro; print(distro.id().title())")
 version=$1
 
 echo "Zipping to Blender_Launcher_${version}_${distro_id}_x64.zip"
-cd ./dist/release/ || exit
-zip "Blender_Launcher_${version}_${distro_id}_x64.zip" "./Blender Launcher" "./extras/blenderlauncher.desktop" "./source/resources/icons/bl/bl_128.png"
-
+zip "Blender_Launcher_${version}_${distro_id}_x64.zip" "./dist/release/Blender Launcher" "./extras/blenderlauncher.desktop" "./source/resources/icons/bl/bl_128.png"
+mv "Blender_Launcher_${version}_${distro_id}_x64.zip" ./dist/release/
