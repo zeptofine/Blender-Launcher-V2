cb8f1e87018358be1fa6fed915631f770b0b2582
Victor <victor.ched@gmail.com>
2024-08-26 21:33:57 -0400

    Add tooltips in the blender Appearance Settings Tab

--- source/widgets/settings_window/blender_builds_tab.py
+++ source/widgets/settings_window/blender_builds_tab.py
@@ -63,7 +63,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.MinStableBlenderVer.addItems(blender_minimum_versions.keys())
         self.MinStableBlenderVer.setToolTip(
             "Minimum stable Blender version to scrape\
-            \nDefault: 3.2"
+            \nDEFAULT: 3.2"
         )
         self.MinStableBlenderVer.setCurrentIndex(get_minimum_blender_stable_version())
         self.MinStableBlenderVer.activated[str].connect(self.change_minimum_blender_stable_version)
@@ -75,7 +75,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.CheckForNewBuildsAutomatically.setText("Check automatically")
         self.CheckForNewBuildsAutomatically.setToolTip(
             "Check for new Blender builds automatically\
-            \nDefault: Off"
+            \nDEFAULT: Off"
         )
         # How often to check for new builds if ^^ enabled
         self.NewBuildsCheckFrequency = QSpinBox()
@@ -83,7 +83,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.NewBuildsCheckFrequency.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
         self.NewBuildsCheckFrequency.setToolTip(
             "Time in hours between new Blender builds check\
-            \nDefault: 12h"
+            \nDEFAULT: 12h"
         )
         self.NewBuildsCheckFrequency.setMaximum(24 * 7 * 4)  # 4 weeks?
         self.NewBuildsCheckFrequency.setMinimum(12)
@@ -98,7 +98,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.CheckForNewBuildsOnStartup.setText("On startup")
         self.CheckForNewBuildsOnStartup.setToolTip(
             "Check for new Blender builds on Blender Launcher startup\
-            \nDefault: On"
+            \nDEFAULT: On"
         )
 
         # Scraping builds settings
@@ -108,7 +108,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.ScrapeStableBuilds.setText("Scrape stable builds")
         self.ScrapeStableBuilds.setToolTip(
             "Scrape stable Blender builds\
-            \nDefault: On"
+            \nDEFAULT: On"
         )
         self.ScrapeAutomatedBuilds = QCheckBox(self)
         self.ScrapeAutomatedBuilds.setChecked(get_scrape_automated_builds())
@@ -116,7 +116,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.ScrapeAutomatedBuilds.setText("Scrape automated builds (daily/experimental/patch)")
         self.ScrapeAutomatedBuilds.setToolTip(
             "Scrape daily, experimental, and patch Blender builds\
-            \nDefault: On"
+            \nDEFAULT: On"
         )
 
         # Show Archive Builds
@@ -124,7 +124,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.show_daily_archive_builds.setText("Show Daily Archived Builds")
         self.show_daily_archive_builds.setToolTip(
             "Show Daily Archived Builds\
-            \nDefault: Off"
+            \nDEFAULT: Off"
         )
         self.show_daily_archive_builds.setChecked(get_show_daily_archive_builds())
         self.show_daily_archive_builds.clicked.connect(self.toggle_show_daily_archive_builds)
@@ -132,7 +132,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.show_experimental_archive_builds.setText("Show Experimental Archived Builds")
         self.show_experimental_archive_builds.setToolTip(
             "Show Experimental Archived Builds\
-            \nDefault: Off"
+            \nDEFAULT: Off"
         )
         self.show_experimental_archive_builds.setChecked(get_show_experimental_archive_builds())
         self.show_experimental_archive_builds.clicked.connect(self.toggle_show_experimental_archive_builds)
@@ -140,7 +140,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.show_patch_archive_builds.setText("Show Patch Archived Builds")
         self.show_patch_archive_builds.setToolTip(
             "Show Patch Archived Builds\
-            \nDefault: Off"
+            \nDEFAULT: Off"
         )
         self.show_patch_archive_builds.setChecked(get_show_patch_archive_builds())
         self.show_patch_archive_builds.clicked.connect(self.toggle_show_patch_archive_builds)
@@ -167,7 +167,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.EnableMarkAsFavorite.setText("Mark as Favorite")
         self.EnableMarkAsFavorite.setToolTip(
             "Mark a tab as favorite to quickly access it\
-            \nDefault: Off"
+            \nDEFAULT: Off"
         )
         self.EnableMarkAsFavorite.setChecked(get_mark_as_favorite() != 0)
         self.EnableMarkAsFavorite.clicked.connect(self.toggle_mark_as_favorite)
@@ -175,7 +175,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.MarkAsFavorite.addItems([fav for fav in favorite_pages if fav != "Disable"])
         self.MarkAsFavorite.setToolTip(
             "Select a tab to mark as favorite\
-            \nDefault: Stable Releases"
+            \nDEFAULT: Stable Releases"
         )
         self.MarkAsFavorite.setCurrentIndex(max(get_mark_as_favorite() - 1, 0))
         self.MarkAsFavorite.activated[str].connect(self.change_mark_as_favorite)
@@ -186,7 +186,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.InstallTemplate.setText("Install Template")
         self.InstallTemplate.setToolTip(
             "Installs a template on newly added builds to the Library tab\
-            \nDefault: Off"
+            \nDEFAULT: Off"
         )
         self.InstallTemplate.clicked.connect(self.toggle_install_template)
         self.InstallTemplate.setChecked(get_install_template())
@@ -205,7 +205,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.EnableQuickLaunchKeySeq.setText("Quick Launch Global Shortcut")
         self.EnableQuickLaunchKeySeq.setToolTip(
             "Enable a global shortcut to quickly launch Blender\
-            \nDefault: On"
+            \nDEFAULT: On"
         )
         self.EnableQuickLaunchKeySeq.clicked.connect(self.toggle_enable_quick_launch_key_seq)
         self.EnableQuickLaunchKeySeq.setChecked(get_enable_quick_launch_key_seq())
@@ -215,7 +215,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.QuickLaunchKeySeq.setText(str(get_quick_launch_key_seq()))
         self.QuickLaunchKeySeq.setToolTip(
             "Global shortcut to quickly launch Blender\
-            \nDefault: ctrl + f11"
+            \nDEFAULT: ctrl + f11"
         )
         self.QuickLaunchKeySeq.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
         self.QuickLaunchKeySeq.setCursorPosition(0)
@@ -225,7 +225,7 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
         self.LaunchBlenderNoConsole.setText("Hide Console On Startup")
         self.LaunchBlenderNoConsole.setToolTip(
             "Hide the console window when launching Blender\
-            \nDefault: On"
+            \nDEFAULT: On"
         )
         self.LaunchBlenderNoConsole.clicked.connect(self.toggle_launch_blender_no_console)
         self.LaunchBlenderNoConsole.setChecked(get_launch_blender_no_console())
@@ -250,14 +250,14 @@ class BlenderBuildsTabWidget(SettingsFormWidget):
             self.launching_layout.addRow(self.BashArguments)
             self.BashArguments.setToolTip(
                 "Instructions to pass to bash when launching Blender (before the Blender executable i.e. [<args> …])\
-                \nDefault: None\
+                \nDEFAULT: None\
                 \nExample: env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia nohup"
             )
         self.launching_layout.addRow(QLabel("Startup Arguments:", self))
         self.launching_layout.addRow(self.BlenderStartupArguments)
         self.BlenderStartupArguments.setToolTip(
             "Arguments to pass to when launching Blender (after the Blender executable i.e. [… <args>]\
-            \nDefault: None\
+            \nDEFAULT: None\
             \nExample: --background"
         )
         self.launching_settings.setLayout(self.launching_layout)
